6246ab97444b43dc368d062e723f5e8bc5e068b8
Sunbuilder2020 <david.jannid.meyer@gmail.com>
2024-01-05 09:49:49 +0100

    Added an unnecessary comment

--- src/main/java/net/sunbuilder/legendary_items/items/custom/AthenasShield.java
+++ src/main/java/net/sunbuilder/legendary_items/items/custom/AthenasShield.java
@@ -16,6 +16,7 @@ import software.bernie.geckolib.util.RenderUtils;
 import java.util.function.Consumer;
 
 public class AthenasShield extends ShieldItem implements GeoItem {
+    //This is very unnecessary
     private AnimatableInstanceCache cache = new SingletonAnimatableInstanceCache(this);
 
     public AthenasShield(Properties pProperties) {
